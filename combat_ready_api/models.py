e57b0c78fbb9dec066033f3bfcaaebfe5aa8033a
ununique3 <j.horst77@gmail.com>
2022-09-18 14:45:45 -0600

    changed numbers to strings in the players model

--- combat_ready_api/models.py
+++ combat_ready_api/models.py
@@ -7,25 +7,22 @@ class Matches(models.Model):
     nameP1 = models.CharField(max_length=40, default = '')
     realNameP1 = models.CharField(max_length=60, default = '')
     speciesP1 = models.CharField(max_length=60, default = '')
-    intelligenceP1 = models.IntegerField(default = 0)
-    strengthP1 = models.IntegerField(default = 0)
-    speedP1 = models.IntegerField(default = 0)
-    durabilityP1 = models.IntegerField(default = 0)
-    powerP1 = models.IntegerField(default = 0)
-    imageP1 = models.TextField(default = '')
+    intelligenceP1 = models.CharField(max_length=5, default = '')
+    strengthP1 = models.CharField(max_length=5, default = '')
+    speedP1 = models.CharField(max_length=5, default = '')
+    durabilityP1 = models.CharField(max_length=5, default = '')
+    powerP1 = models.CharField(max_length=5, default = '')
+    imageP1 = models.TextField(max_length=5, default = '')
 
     nameP2 = models.CharField(max_length=40, default = '')
     realNameP2 = models.CharField(max_length=60, default = '')
     speciesP2 = models.CharField(max_length=60, default = '')
-    intelligenceP2 = models.IntegerField(default = 0)
-    strengthP2 = models.IntegerField(default = 0)
-    speedP2 = models.IntegerField(default = 0)
-    durabilityP2 = models.IntegerField(default = 0)
-    powerP2 = models.IntegerField(default = 0)
+    intelligenceP2 = models.CharField(max_length=5, default = '')
+    strengthP2 = models.CharField(max_length=5, default = '')
+    speedP2 = models.CharField(max_length=5, default = '')
+    durabilityP2 = models.CharField(max_length=5, default = '')
+    powerP2 = models.CharField(max_length=5, default = '')
     imageP2 = models.TextField(default = '')
 
 class Stage(models.Model):
     nameOfStage = models.CharField(max_length=100, default='0000000')
-
-    
-
